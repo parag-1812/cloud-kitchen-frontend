04bc387beb240d6d37b73c4bb225d79451cabfe8
parag-1812 <paraggupta9988@gmail.com>
2026-03-06 18:43:37 +0530

    add admin management APIs and analytics dashboard

---
+++ src/api/adminApi.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+api.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export const getStats = async () => {
+  const res = await api.get("/admin/stats");
+  return res.data;
+};
+
+export const getAllOrders = async () => {
+  const res = await api.get("/admin/orders");
+  return res.data;
+};
