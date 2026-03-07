577166afae17cc1f809f14f7f7975aad3b44ec76
parag-1812 <paraggupta9988@gmail.com>
2026-03-07 23:50:31 +0530

    Changes

--- src/api/adminApi.js
+++ src/api/adminApi.js
@@ -4,21 +4,7 @@ const api = axios.create({
   baseURL: "http://localhost:8080",
 });
 
-api.interceptors.request.use((config) => {
 
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
-export const getStats = async () => {
-  const res = await api.get("/admin/stats");
-  return res.data;
-};
 
 export const getAllOrders = async () => {
   const res = await api.get("/admin/orders");
